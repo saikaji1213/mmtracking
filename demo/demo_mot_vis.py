7a19c5abb02635f7a532e6cb73fe2640212a4d32
saikaji1213 <shenjk@oiine.com>
2023-12-10 15:45:50 +0800

    chore: vehicle&license detection
    utils for color recognition

--- demo/demo_mot_vis.py
+++ demo/demo_mot_vis.py
@@ -5,8 +5,27 @@ import tempfile
 from argparse import ArgumentParser
 
 import mmcv
+import numpy as np
 
 from mmtrack.apis import inference_mot, init_model
+from icecream import ic
+import hyperlpr3 as lpr3
+
+import cv2
+from PIL import ImageFont
+from PIL import Image
+from PIL import ImageDraw
+
+
+def draw_plate_on_image(img, box, text, font):
+    x1, y1, x2, y2 = box
+    # cv2.rectangle(img, (x1, y1), (x2, y2), (139, 139, 102), 2, cv2.LINE_AA)
+    # cv2.rectangle(img, (x1, y1 - 20), (x2, y1), (139, 139, 102), -1)
+    data = Image.fromarray(img)
+    draw = ImageDraw.Draw(data)
+    draw.text((x1 + 5, y1 - 20), text, (255, 255, 255), font=font)
+    res = np.asarray(data)
+    return res
 
 
 def main():
@@ -70,12 +89,50 @@ def main():
     # build the model from a config file and a checkpoint file
     model = init_model(args.config, args.checkpoint, device=args.device)
 
+    # ic(len(model.CLASSES))
+    # ic(model.CLASSES)
+
+    font_ch = ImageFont.truetype("./demo/font/platech.ttf", 20, 0)
+    catcher = lpr3.LicensePlateCatcher(detect_level=lpr3.DETECT_LEVEL_HIGH)
+    car_track_license = {}
+    car_track_color = {}
+
     prog_bar = mmcv.ProgressBar(len(imgs))
     # test and show/save the images
     for i, img in enumerate(imgs):
         if isinstance(img, str):
             img = osp.join(args.input, img)
         result = inference_mot(model, img, frame_id=i)
+
+        # dealing result
+        # ic(result['det_bboxes'])
+        car_refer_idxs = [68, 71, 85, 122, 186, 196, 232, 264, 272, 309, 331, 350, 444, 449]
+        for idx in range(len(result['det_bboxes'])):
+            if idx not in car_refer_idxs:
+                result['det_bboxes'][idx] = np.zeros((0, 5), dtype='float32')
+                result['track_bboxes'][idx] = np.zeros((0, 6), dtype='float32')
+            else:
+                # track_id不变期间识别截取track_bbox识别车牌，识别到不再识别
+                for track_bbox in result['track_bboxes'][idx]:
+                    if not track_bbox[-1] > args.score_thr:
+                        continue
+                    if track_bbox[0] in car_track_license:
+                        if car_track_license[track_bbox[0]] is None or "":
+                            bbox = track_bbox[1:-1]
+                            car_image = mmcv.imcrop(img, bbox)
+                            license_results = catcher(car_image)  # list(code, confidence, type_idx, box)
+                            if license_results:
+                                license_result_idx = np.argmax(np.asarray(license_results, dtype=object), axis=0)[1]
+                                car_track_license[track_bbox[0]] = f"{license_results[license_result_idx][0]}"
+                    else:
+                        car_track_license[track_bbox[0]] = None
+                    text = car_track_license[track_bbox[0]]
+                    if text is None:
+                        text = 'Unknown'
+                    img = draw_plate_on_image(img, track_bbox[1:-1], text, font=font_ch)
+                    img = np.array(img)
+                    img.flags.writeable = True
+
         if args.output is not None:
             if IN_VIDEO or OUT_VIDEO:
                 out_file = osp.join(out_path, f'{i:06d}.jpg')
@@ -98,6 +155,7 @@ def main():
         mmcv.frames2video(out_path, args.output, fps=fps, fourcc='mp4v')
         out_dir.cleanup()
 
+    ic(car_track_license)
 
 if __name__ == '__main__':
     main()
